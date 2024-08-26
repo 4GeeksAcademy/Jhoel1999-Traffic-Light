96eff69a4e89671460f6a3b7982cc484ce5afbdd
JhoelSand <126299517+Jhoel1999@users.noreply.github.com>
2024-08-26 02:51:58 +0000

    traffic light functionalities

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState ("red");
+	const [divPurpura, setDivPurpura] = useState(false);
+
+	const handleColor = () => {
+		if (color === "red") {
+			setColor("yellow")
+		} else if(color === "yellow"){
+			setColor("green")
+		}else if (color === "green" && divPurpura === true) {
+			setColor("purpura")
+		}else{
+			setColor("red")
+		}
+	}
+
+	const handlePurpura = () => {
+		if (divPurpura === false){
+			setDivPurpura(true)
+		}else{
+			setDivPurpura(false)
+		}
+		
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="d-inline-block bg-dark p-2 m-2 rounded">
+				<div onClick={()=> setColor("red")} 
+					className={" light bg-danger rounded-circle mb-2" + (color === "red" ? " glow" : "")} 
+				>
+				</div>
+				<div onClick={()=> setColor("yellow")}
+					className={"light bg-warning rounded-circle mb-2" + (color === "yellow" ? " glow" : "")} 
+				>
+				</div>
+				<div onClick={()=> setColor("green")}
+					className={"light bg-success rounded-circle mb-2" + (color === "green" ? " glow" : "")} 
+				>
+				</div>
+				{divPurpura && (
+						<div
+							onClick={() => setColor("purpura")}
+							className={
+								"light rounded-circle mb-2" +
+								(color === "purpura" ? " glow" : "")
+							}
+							style={{ backgroundColor: "#6f42c1" }}
+						></div>
+					)}
+			</div>
+			<div className="d-flex p-2">
+				<button onClick={handleColor} className="btn btn-secondary">Cambiar</button>
+				<button onClick={handlePurpura} className="btn btn-primary ms-2">Boton Purpura</button>
+			</div>
+		</>
+		
 	);
 };
 
